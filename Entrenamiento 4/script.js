a2327801a7e6238c9b319040a93f073cfb63ff91
juaanlopeez01 <114492371+juaanlopeez01@users.noreply.github.com>
2024-10-03 18:32:09 +0200

    formulario con pequeños cambios pero terminado

--- Entrenamiento 4/script.js
+++ Entrenamiento 4/script.js
@@ -1,33 +1,52 @@
-const inputs = document.querySelectorAll('input, select');
+const inputs = document.querySelectorAll('input:not([id="tipoVia"]), select:not([id="tipoVia"])');
 
 inputs.forEach(input => {
-  input.addEventListener('blur', (e) => {
-    const inputType = input.type;
-    const inputValue = input.value;
-    const pattern = getPattern(inputType);
-    const errorMessage = getErrorMessage(inputType);
-    
-    if (!validateInput(inputValue, pattern)) {
-      input.classList.add('error');
-      input.placeholder = errorMessage;
+    input.addEventListener('blur', (e) => {
+        const inputType = input.type;
+        const inputValue = input.value.trim();
+        const pattern = getPattern(input);
+
+    if (inputValue === "") {
+        input.classList.add('error');
+        input.setCustomValidity('Este campo no puede estar vacío.');
+    } else if (!validateInput(inputValue, pattern)) {
+        input.classList.add('error');
+        input.setCustomValidity('Formato inválido.');
     } else {
-      input.classList.remove('error');
-      input.placeholder = '';
+        input.classList.remove('error');
+        input.setCustomValidity(''); 
+        if (input.classList.contains('nombre') || input.classList.contains('apellido') || input.classList.contains('direccion') || input.classList.contains('dni')) {
+            input.value = inputValue.toUpperCase();
+        } 
     }
-  });
+    });
 });
 
-function getPattern(inputType) {
-  switch (inputType) {
-    case 'text':
-        if (inputType === 'nombre' || inputType === 'apellido' || inputType === 'direccion') {
+function getPattern(input) {
+    const inputType = input.type;
+    switch (inputType) {
+        case 'text':
+            if (input.classList.contains('nombre') || input.classList.contains('apellido') || input.classList.contains('direccion') ) {
+                return /^[A-Z\s]+$/i;
+            } else if (input.classList.contains('dni')) {
+                return /^\d{8}[A-Z]$/i;
+            }
+            break;
+
+        case 'email':
+            return /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/;
+
+        case 'tel':
+            return /^\d{9}$/; 
+
+        case 'number':
+            return /^(0|[1-9][0-9]?|1[01][0-9]|120)$/; 
+
+        default:
             return;
-        }
-    case 'email':
-        return /^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/;
-    case 'tel':
-        return /^\d{9}$/;
-    case 'number':
-        return /^(0|[1-9][0-9]?|1[01][0-9]|120)$/;
-  }
-};
+      }
+}
+
+function validateInput(value, pattern) {
+    return pattern.test(value);
+}
