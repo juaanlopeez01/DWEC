4738ab64f2276c8698ec5520cc03c9080085da90
juaanlopeez01 <114492371+juaanlopeez01@users.noreply.github.com>
2024-11-18 15:33:19 +0100

    pequeño cambio

--- actividad2Blog/src/app/blog/blog.component.ts
+++ actividad2Blog/src/app/blog/blog.component.ts
@@ -3,68 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: './blog.component.css'
 })
 export class BlogComponent {
   // Array para las noticias iniciales
-  news = [
-    {
-      title: 'Primera noticia',
-      imageUrl: 'https://via.placeholder.com/150',
-      body: 'Este es el cuerpo de la primera noticia.',
-      date: '2024-11-15'
-    },
-    {
-      title: 'Segunda noticia',
-      imageUrl: 'https://via.placeholder.com/150',
-      body: 'Este es el cuerpo de la segunda noticia.',
-      date: '2024-11-16'
+  arrAficiones: Iaficiones [] = [];
+
+  newAficion: Iaficiones = {
+    aficion: "",
+    nombre: ""
+  };
+
+  guardarDatos():void{
+    //Guardar los datos
+    this.arrAficiones.push(this.newAficion);
+
+    this.newAficion = {
+      aficion: "",
+      nombre: "",
     }
-  ];
-
-  // Propiedades del formulario
-  newTitle: string = '';
-  newImageUrl: string = '';
-  newBody: string = '';
-  newDate: string = '';
-
-  // Método para agregar una nueva noticia
-  addNews() {
-    if (!this.newTitle || !this.newImageUrl || !this.newBody || !this.newDate) {
-      alert('Todos los campos son obligatorios');
-      return;
-    }
-
-    const newPost = {
-      title: this.newTitle,
-      imageUrl: this.newImageUrl,
-      body: this.newBody,
-      date: this.newDate
-    };
-
-    this.news.push(newPost);
-
-    // Limpiar los campos del formulario
-    this.newTitle = '';
-    this.newImageUrl = '';
-    this.newBody = '';
-    this.newDate = '';
+    console.log(this.arrAficiones);
   }
 
-  // Métodos para capturar los datos del formulario de manera manual (sin ngModel)
-  onTitleChange(event: any) {
-    this.newTitle = event.target.value;
-  }
-
-  onImageUrlChange(event: any) {
-    this.newImageUrl = event.target.value;
-  }
-
-  onBodyChange(event: any) {
-    this.newBody = event.target.value;
-  }
-
-  onDateChange(event: any) {
-    this.newDate = event.target.value;
+  cargarDatos(): string {
+    let html : string = "";
+    this.arrAficiones.forEach(element => {
+      html += `<p>${element.aficion} - ${element.nombre}</p>`
+    });
+    return html;
   }
 }
